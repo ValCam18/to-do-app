0ed2053073bf4f225484afde8a0caa714eae6dff
ValCam18 <valeria-camila18@hotmail.com>
2025-01-21 14:54:36 -0500

    se agregó comentario

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// ESTO ES editarTarea
+//ESTO ES EDITAR TAREA
 window.onload = function() {
     // Obtener las categorías desde el localStorage
     var categorias = JSON.parse(localStorage.getItem('categorias')) || [];
